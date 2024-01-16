cdf6484ffd3af0908251cb2894561d2dd9a4d324
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-01-16 00:09:06 -0300

    Cria o banco de dados

--- catalogoDeVeiculos/models.py
+++ catalogoDeVeiculos/models.py
@@ -1,1 +1,17 @@
-# cria a estrutura do banco de dados
+from catalogoDeVeiculos import database
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+
+class Veiculo(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    nome = database.Column(database.String, nullable=False)
+    marca = database.Column(database.String, nullable=False)
+    modelo = database.Column(database.String, nullable=False)
+    ano = database.Column(database.String, nullable=False)
+    cor = database.Column(database.String, nullable=False)
+    preco = database.Column(database.Integer, nullable=False)
+    foto = database.Column(database.String, default="default.png")
